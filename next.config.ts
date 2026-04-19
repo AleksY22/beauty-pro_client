86ad810b0c0ee09805bddb58e30f1f694b6aae0e
AleksY22 <71137788+AleksY22@users.noreply.github.com>
2026-04-19 15:20:38 +0300

    feat: initial commit

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,42 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  devIndicators: false,
+
+  env: {
+    APP_ENV: process.env.APP_ENV,
+    APP_URL: process.env.APP_URL,
+    APP_DOMAIN: process.env.APP_DOMAIN,
+    SERVER_URL: process.env.SERVER_URL,
+    GOOGLE_RECAPTCHA_SITE_KEY: process.env.GOOGLE_RECAPTCHA_SITE_KEY,
+  },
+
+  images: {
+    //Для доступу к изображениям на локальном сервере
+    dangerouslyAllowLocalIP: true,
+
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'avatars.yandex.net',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'beauty-pro-server.vercel.app',
+      },
+      { hostname: '*.public.blob.vercel-storage.com' },
+      {
+        protocol: 'https',
+        hostname: 'beauty-pro-server.vercel.app',
+        port: '4000',
+        pathname: '/uploads/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
---
+++ src/shared/constants/seo.constants.ts
@@ -0,0 +1,4 @@
+export const NO_INDEX_PAGE = { robots: { index: false, follow: false } };
+
+export const SITE_NAME = "Beauty Pro";
+export const SITE_DESCRIPTION = "Интернет-магазин по продаже косметики";
