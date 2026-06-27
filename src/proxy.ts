9f75d7b9a873927dfa32081b6b11c8ffdb391f80
AleksY22 <71137788+AleksY22@users.noreply.github.com>
2026-06-27 15:31:47 +0300

    disable proxy

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,33 +1,33 @@
-import { NextRequest, NextResponse } from 'next/server';
+// import { NextRequest, NextResponse } from 'next/server';
 
-import { PUBLIC_URL } from './shared/config/url.config';
+// import { PUBLIC_URL } from './shared/config/url.config';
 
-export default function proxy(request: NextRequest) {
-   const { nextUrl, cookies } = request;
+// export default function proxy(request: NextRequest) {
+//    const { nextUrl, cookies } = request;
 
-   const session = cookies.get('session')?.value;
+//    const session = cookies.get('session')?.value;
 
-   const isAuthPage = nextUrl.pathname.startsWith(PUBLIC_URL.auth());
+//    const isAuthPage = nextUrl.pathname.startsWith(PUBLIC_URL.auth());
 
-   if (isAuthPage) {
-      if (session) {
-         return NextResponse.redirect(new URL(PUBLIC_URL.home(), nextUrl));
-      }
+//    if (isAuthPage) {
+//       if (session) {
+//          return NextResponse.redirect(new URL(PUBLIC_URL.home(), nextUrl));
+//       }
 
-      return NextResponse.next();
-   }
+//       return NextResponse.next();
+//    }
 
-   // 2. ОПРЕДЕЛЯЕМ ПРИВАТНЫЕ ЗОНЫ (Где гостям быть категорически нельзя)
-   const isDashboardPage = nextUrl.pathname.startsWith('/dashboard');
-   const isAdminPage = nextUrl.pathname.startsWith('/admin');
+//    // 2. ОПРЕДЕЛЯЕМ ПРИВАТНЫЕ ЗОНЫ (Где гостям быть категорически нельзя)
+//    const isDashboardPage = nextUrl.pathname.startsWith('/dashboard');
+//    const isAdminPage = nextUrl.pathname.startsWith('/admin');
 
-   // 3. Если страница приватная, а сессии у пользователя нет — отправляем авторизоваться
-   if ((isDashboardPage || isAdminPage) && !session) {
-      return NextResponse.redirect(new URL('/auth/login', nextUrl));
-   }
+//    // 3. Если страница приватная, а сессии у пользователя нет — отправляем авторизоваться
+//    if ((isDashboardPage || isAdminPage) && !session) {
+//       return NextResponse.redirect(new URL('/auth/login', nextUrl));
+//    }
 
-   return NextResponse.next();
-}
+//    return NextResponse.next();
+// }
 
 // export const config = {
 //    matcher: ['/auth/:path*', '/dashboard/:path*', '/admin/:path*'],
