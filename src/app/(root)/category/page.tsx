764f321091a3c570c08245a7e76118bc82aaa38f
AleksY22 <71137788+AleksY22@users.noreply.github.com>
2026-06-27 13:09:41 +0300

    del sus

--- src/app/(root)/category/page.tsx
+++ src/app/(root)/category/page.tsx
@@ -1,5 +1,4 @@
 import { Metadata } from 'next';
-import { Suspense } from 'react';
 
 import { CatalogProducts } from './catalog-products';
 import { productVariantService } from '@/features/product-variant/services/product-variant.service';
@@ -17,9 +16,5 @@ async function getVariants() {
 
 export default async function CatalogPage() {
    const data = await getVariants();
-   return (
-      <Suspense fallback={<div>Загрузка товаров категории...</div>}>
-         <CatalogProducts variants={data} />
-      </Suspense>
-   );
+   return <CatalogProducts variants={data} />;
 }
