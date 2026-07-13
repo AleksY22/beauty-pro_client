0f37baafd7b8b23684d632a3a69ad742f0b7d5fd
AleksY22 <71137788+AleksY22@users.noreply.github.com>
2026-07-13 23:26:37 +0300

    fix catalog button

--- src/shared/components/main-layout/header/catalog-button.tsx
+++ src/shared/components/main-layout/header/catalog-button.tsx
@@ -2,32 +2,38 @@
 
 import { Button } from '../../ui';
 import { Grip, Loader2 } from 'lucide-react';
-import Link from 'next/link';
-import { useState } from 'react';
+import { usePathname, useRouter } from 'next/navigation';
+import { useTransition } from 'react';
 
 import { PUBLIC_URL } from '@/shared/config/url.config';
 
 export function CatalogButton() {
-   const [isLoading, setIsLoading] = useState(false);
+   const router = useRouter();
+   const pathname = usePathname();
+   const [isPending, startTransition] = useTransition();
+
+   const handleNavigate = () => {
+      if (pathname === PUBLIC_URL.category()) return;
+
+      startTransition(() => {
+         router.push(PUBLIC_URL.category());
+      });
+   };
 
    return (
       <Button
-         asChild
-         className={`flex gap-2 bg-red-300 hover:bg-red-300/90 text-amber-50 font-bold text-lg tracking-widest px-6 py-5 rounded-[5px] h-auto transition-all
-        ${isLoading ? 'pointer-events-none opacity-70' : ''}
-      `}
+         onClick={handleNavigate}
+         disabled={isPending}
+         className="flex gap-2 bg-red-300 hover:bg-red-300/70 text-amber-50 font-bold text-lg tracking-widest px-4 py-3 rounded-[5px] h-auto disabled:pointer-events-none disabled:opacity-70 transition-all"
       >
-         <Link
-            href={PUBLIC_URL.category()}
-            onClick={() => setIsLoading(true)} // Включаем режим загрузки при клике
-         >
-            {isLoading ? (
-               <Loader2 className="h-5 w-5 animate-spin" />
-            ) : (
-               <Grip className="h-5 w-5" />
-            )}
-            <span className="hidden sm:inline">КАТАЛОГ</span>
-         </Link>
+         {isPending ? (
+            <Loader2 className="h-5 w-5 animate-spin" />
+         ) : (
+            <Grip className="h-5 w-5" />
+         )}
+
+         {/* Динамическая смена текста */}
+         <span className="hidden sm:inline">КАТАЛОГ</span>
       </Button>
    );
 }
